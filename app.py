6e5ca6642a31adcee582a6b452ceeae42de73479
Frank <fgubars@college.harvard.edu>
2024-11-30 19:54:57 -0500

    Started app.py development
    Introduced basic Flask functionality

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
