50d959970942507f8c5b5c21ffa020fa0eb6f608
fgubars@college.harvard.edu <fgubars@college.harvard.edu>
2024-12-01 21:02:18 -0500

    Added static folder with styles.css

--- app.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
-
-app = Flask(__name__)
